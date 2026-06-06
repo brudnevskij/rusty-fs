09eeceabd6866f3740e14557700b11922a8672c3
Segfaulter <72930656+brudnevskij@users.noreply.github.com>
2026-06-07 00:36:20 +0300

    feat: added InitError to the FsError

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,9 @@ use thiserror::Error;
 pub enum FsError {
     #[error("disk error {0}")]
     DiskError(#[from] DiskError),
+
+    #[error("init error: {0}")]
+    InitError(String),
 }
 
 pub type FsResult<T> = Result<T, FsError>;
